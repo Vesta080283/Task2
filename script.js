d405a01e90cbeed9147dd1481d2d5056a8c6bf36
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2019-05-18 11:23:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 'use strict'
+window.onload = () => {
+    const getSteps = () => {
+        let currentPosition = document.querySelector('#curPos').value.toUpperCase();
+        let curX = currentPosition.charCodeAt(0);
+        let curY = currentPosition.slice(1);
+        let x;
+        let y;
+        let result = [];
 
-const getSteps = () => {
-    let currentPosition = document.querySelector('#curPos').value.toUpperCase();
-    let curX = currentPosition.charCodeAt(0);
-    //let curY = currentposition.charCodeAt(1);
-    let curY = currentPosition.slice(1);
-    let x;
-    let y;
-    let result = [];
+        ///создадим массив траекторий хода
+        let steps = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
 
-    ///создадим массив траекторий хода
-    let steps = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
+        steps.forEach((step) => {
+            if (((x = curX + step[0]) >= ("A".charCodeAt(0)) &&
+                (x = curX + step[0]) <= ("H".charCodeAt(0))) &&
+                ((y = +curY + step[1]) >= 1 &&
+                (y = +curY + step[1]) <= 8)) {
+                    result.push(String.fromCharCode(x) + y);
+                }
+        });
 
-    steps.forEach((step) => {
-        if (((x = curX + step[0]) >= ("A".charCodeAt(0)) &&
-            (x = curX + step[0]) <= ("H".charCodeAt(0))) &&
-            ((y = +curY + step[1]) >= 1 &&
-            (y = +curY + step[1]) <= 8)) {
-                result.push(String.fromCharCode(x) + y);
-            }
-    });
-
-    if (result.length) {
-        alert('Результат: ' + result.join(' '));
-    } else {
-        alert('Результат: нет допустимых ходов!');
+        if (result.length) {
+            alert('Результат: ' + result.join(' '));
+        } else {
+            alert('Результат: нет допустимых ходов!');
+        }
     }
 }
