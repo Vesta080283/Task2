d354c53341be7c00e64b1b67360b1ce3725607bb
Vesta080283 <43104215+Vesta080283@users.noreply.github.com>
2019-05-20 08:47:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 'use strict'
-window.onload = () => {
-    const getSteps = () => {
-        let currentPosition = document.querySelector('#curPos').value.toUpperCase();
-        let curX = currentPosition.charCodeAt(0);
-        let curY = currentPosition.slice(1);
-        let x;
-        let y;
-        let result = [];
 
-        ///создадим массив траекторий хода
-        let steps = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
+const getSteps = () => {
+    let currentPosition = document.querySelector('#curPos').value.toUpperCase();
+    let curX = currentPosition.charCodeAt(0);
+    //let curY = currentposition.charCodeAt(1);
+    let curY = currentPosition.slice(1);
+    let x;
+    let y;
+    let result = [];
 
-        steps.forEach((step) => {
-            if (((x = curX + step[0]) >= ("A".charCodeAt(0)) &&
-                (x = curX + step[0]) <= ("H".charCodeAt(0))) &&
-                ((y = +curY + step[1]) >= 1 &&
-                (y = +curY + step[1]) <= 8)) {
-                    result.push(String.fromCharCode(x) + y);
-                }
-        });
+    ///создадим массив траекторий хода
+    let steps = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]];
 
-        if (result.length) {
-            alert('Результат: ' + result.join(' '));
-        } else {
-            alert('Результат: нет допустимых ходов!');
-        }
+    steps.forEach((step) => {
+        if (((x = curX + step[0]) >= ("A".charCodeAt(0)) &&
+            (x = curX + step[0]) <= ("H".charCodeAt(0))) &&
+            ((y = +curY + step[1]) >= 1 &&
+            (y = +curY + step[1]) <= 8)) {
+                result.push(String.fromCharCode(x) + y);
+            }
+    });
+
+    if (result.length) {
+        alert('Возможные варианты хода: ' + result.join(' '));
+    } else {
+        alert('Нет допустимых ходов!');
     }
 }
